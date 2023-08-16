3fbc3e8ae2d90997e816ef8c60fd8b6eafb27579
ArthurOReis <arthuroreis2005@gmail.com>
2023-08-15 22:35:56 -0300

    Criando componentes da cesta

---
+++ src/Telas/Cesta/components/Topo.jsx
@@ -0,0 +1,32 @@
+import { Dimensions, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import Texto from '../../../components/Texto';
+import topo from '../../../../assets/topo.png'
+
+const width = Dimensions.get('screen').width;
+
+export default function Topo() {
+  return (
+    <>
+        <Image source={topo} style={estilos.topo} />
+        <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: '100%',
+        height: 578 / 768 * width,
+      },
+      titulo: {
+        position: "absolute",
+        textAlign: "center",
+        width: '100%',
+        fontSize: 16,
+        lineHeight: 26,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: 16
+      },
+});
